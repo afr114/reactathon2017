cff582b7429e39cafc3e3e31297c3f462766a656
afr114 <afr114@gmail.com>
2017-03-12 10:36:48 -0700

    add mock listings

---
+++ src/listings.js
@@ -0,0 +1,58 @@
+export const listings = [
+  {
+    rid: 83236,
+    img: {
+      imgUrl: 'https://resizer.otstatic.com/v1/fDzI465IL8%2B687zSupRS4Q/23698358.jpg',
+      alt: 'Bouche'
+    }
+    restaurantName: 'Bouche',
+    location: 'Downtown / Union Square',
+    type: 'French',
+  },
+  {
+    rid: 3592,
+    img: {
+      imgUrl: 'https://resizer.otstatic.com/v1/fDzI465IL8%2B687zSupRS4Q/24606526.jpg',
+      alt: 'Cafe Claude'
+    }
+    restaurantName: 'Cafe Claude',
+    location: 'Downtown / Union Square',
+    type: 'French'
+  },
+  { rid: 2989,
+    img: {
+      imgUrl: 'https://resizer.otstatic.com/v1/fDzI465IL8%2B687zSupRS4Q/23681388.jpg',
+      alt: 'MKT Restaurant and Bar'
+    }
+    restaurantName: 'MKT Restaurant and Bar',
+    location: 'Downtown / Union Square',
+    type: 'Contemporary American'
+  },
+  { rid: 24886,
+    img: {
+      imgUrl: 'https://resizer.otstatic.com/v1/fDzI465IL8%2B687zSupRS4Q/23679726.jpg',
+      alt: 'Amber India'
+    }
+    restaurantName: 'Amber India',
+    location: 'Downtown / Union Square',
+    type: 'India'
+  },
+  { rid: 141,
+    img: {
+      imgUrl: 'https://resizer.otstatic.com/v1/fDzI465IL8%2B687zSupRS4Q/23672428.jpg',
+      alt: 'ANZU',
+    }
+    restaurantName: 'ANZU',
+    location: 'Downtown / Union Square',
+    type: 'Californian'
+  },
+  { rid: 3598,
+    img: {
+      imgUrl: 'https://resizer.otstatic.com/v1/fDzI465IL8%2B687zSupRS4Q/23672428.jpg',
+      alt: 'B44',
+    }
+    restaurantName: 'B44',
+    location: 'Downtown / Union Square',
+    type: 'Spanish'
+  },
+]
