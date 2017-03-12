3bcfa19484bb0998a618fccbe6ec40ad9ef5d891
Karine Trepanier <trekare@gmail.com>
2017-03-12 01:53:33 -0800

    Added updating logic

--- src/components/DiscountsTable/DiscountsTable.js
+++ src/components/DiscountsTable/DiscountsTable.js
@@ -1,15 +1,90 @@
 import React, { Component } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { Button, Glyphicon } from 'react-bootstrap';
+import Multiselect from 'react-bootstrap-multiselect';
+
+import './DiscountsTable.css';
+
+const daysFormatter = (cell, row) => (<span>{ (cell || []).join(', ') }</span>);
+const createDaysEditor = (onUpdate, props) => (<DaysEditor onUpdate={ onUpdate } {...props}/>);
+const days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
+
+
+class DaysEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.updateData = this.updateData.bind(this);
+    this.state = {
+      days: props.defaultValue,
+    };
+    this.onToggleDay = this.onToggleDay.bind(this);
+  }
+
+  focus() {
+  }
+
+  onToggleDay(day) {
+    if (this.state.days.indexOf(day) < 0) {
+      this.setState({ days: this.state.days.concat([ day ]) });
+    } else {
+      this.setState({ days: this.state.days.filter(r => r !== day) });
+    }
+  }
+
+  updateData() {
+    this.props.onUpdate(this.state.days);
+  }
+
+  render() {
+    // @TODO add animation
+    const dayCheckBoxes = days.map(day => {
+      const styleName = `day-item ${(this.state.days.indexOf(day) > -1) ? 'active' : ''}`;
+      return (
+        <div key={ `span-${day}` } className={styleName}>
+          <label className="day-label" key={ `label-${day}` } htmlFor={ day } onClick={() => this.onToggleDay(day)}>
+            <input
+              className="day-checkbox"
+              type='checkbox'
+              key={ day }
+              name={ day }
+              checked={ this.state.days.indexOf(day) > -1 }
+            />
+            { day }
+          </label>
+        </div>
+      );
+    });
+    return (
+      <div className="day-wrapper" ref='inputRef'>
+        { dayCheckBoxes }
+        <button
+          className='btn btn-info btn-xs textarea-save-btn'
+          onClick={ this.updateData }>
+          save
+        </button>
+      </div>
+    );
+  }
+}
 
 class DiscountsTable extends Component {
   render() {
+    const cellEditProp = {
+      mode: 'click'
+    };
+
+    const updateButton = (cell, row) => (
+      <Button bsStyle="warning" onClick={() => this.props.update(cell)}><Glyphicon glyph="edit" /></Button>
+    );
+
     // @TODO Add tooltips to explain what the columns mean
-    // @TODO Select different days logic
     return (
-      <BootstrapTable data={this.props.discounts} striped={true} hover={true}>
-        <TableHeaderColumn dataField="dayOfWeek" isKey={true} dataAlign="center">Day of the Week</TableHeaderColumn>
+      <BootstrapTable data={this.props.discounts} striped={true} hover={true} cellEdit={cellEditProp}>
+        <TableHeaderColumn dataField="dayOfWeek" dataAlign="center" dataFormat={ daysFormatter }
+            customEditor={ { getElement: createDaysEditor } } editColumnClassName="day-dropdown">Day of the Week</TableHeaderColumn>
         <TableHeaderColumn dataField="percentActivated" dataAlign="center">Occupancy %</TableHeaderColumn>
         <TableHeaderColumn dataField="percentDiscount" dataAlign="center">Discount %</TableHeaderColumn>
+        <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" editable={ false } dataFormat={ updateButton }></TableHeaderColumn>
       </BootstrapTable>
     );
   }
--- src/containers/BusinessContainer/BusinessContainer.js
+++ src/containers/BusinessContainer/BusinessContainer.js
@@ -11,7 +11,7 @@ class BusinessContainer extends Component {
     this.state = {
       showModal: false,
       formInput: {
-        title: '', 
+        title: '',
         dayOfWeek: '',
         percentActivated: '',
         percentDiscount: ''
@@ -52,8 +52,8 @@ class BusinessContainer extends Component {
     // overwrite the property that is being updated and merge the reset of the properties
     const formInput = Object.assign({}, this.state.formInput, update);
     this.setState({ formInput } );
-    
-    
+
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
 
 
@@ -61,7 +61,7 @@ class BusinessContainer extends Component {
     var formData = Object.assign({}, this.state.formInput, {text: '', rid: '12345'});
     console.log(formData);
     request.post({
-      method:'POST', url:'https://dyftmauijc.execute-api.us-east-1.amazonaws.com/dev/deals', body:JSON.stringify(formData), json:true, withCredentials: true}, 
+      method:'POST', url:'https://dyftmauijc.execute-api.us-east-1.amazonaws.com/dev/deals', body:JSON.stringify(formData), json:true, withCredentials: true},
       function(resp) {
         console.log(resp);
         this.toggleModal()
@@ -77,13 +77,18 @@ class BusinessContainer extends Component {
 //    this.setState({ business });
   }
 
+  handleUpdate() {
+    console.log('UPDATING LOGIC');
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <h1>{this.state.business.name}</h1>
-        <Button bsStyle="success" onClick={this.toggleModal}><Glyphicon glyph="plus" /> New</Button>
+        <Button bsStyle="success" onClick={this.toggleModal} className="create-button"><Glyphicon glyph="plus" /> New Discount</Button>
         <DiscountsTable
           discounts={this.state.business.discounts}
+          update={this.handleUpdate}
         />
         <Modal show={this.state.showModal} onHide={this.close}>
           <Modal.Header closeButton>
