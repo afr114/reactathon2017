68b6875fb6b81ce3504cd6d65c0c25b854157017
Karine Trepanier <trekare@gmail.com>
2017-03-11 18:36:16 -0800

    Create Button UI

--- src/components/DiscountsTable/DiscountsTable.js
+++ src/components/DiscountsTable/DiscountsTable.js
@@ -4,6 +4,7 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 class DiscountsTable extends Component {
   render() {
     // @TODO Add tooltips to explain what the columns mean
+    // @TODO Select different days logic
     return (
       <BootstrapTable data={this.props.discounts} striped={true} hover={true}>
         <TableHeaderColumn dataField="dealDayOfWeek" isKey={true} dataAlign="center">Day of the Week</TableHeaderColumn>
